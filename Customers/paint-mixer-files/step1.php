e42b7aa82757149bced2e6e2a9f1fc74ba91e887
Charles Lance <lancegula05@gmail.com>
2024-04-14 08:24:01 +0800

    may bug pa to HAHAHAHA

--- Customers/paint-mixer-files/step1.php
+++ Customers/paint-mixer-files/step1.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CML Paint Trading</title>
+    <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="step1.css" />
+
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
 </head>
 <body>
     
+
 </body>
 </html>
