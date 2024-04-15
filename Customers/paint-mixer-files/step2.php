57af51d385005ebb8bab79bb64b26e1102991fb5
Charles Lance <lancegula05@gmail.com>
2024-04-16 06:57:36 +0800

    Update Step1 and Step2 UI

---
+++ Customers/paint-mixer-files/step2.php
@@ -0,0 +1,40 @@
+<div class="progress-container">
+    <nav class="progress-bar">
+        <ul class="">
+            <li>
+                <a class="" href="">1</a>
+                <span>Select Photo</span>
+            </li>
+            <li>
+                <a href="">2</a>
+                <span>Select Color</span>
+            </li>
+            <li>
+                <a href="" >3</a>
+                <span>Visualize Room</span>
+            </li>
+            <li>
+                <a href="" >4</a>
+                <span>Download Paint</span>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="select-color-container">
+        <a href="#">< Select a Different Color</a>
+        <h1>Find Colors for Your Photo</h1>
+
+        <div class="pick-color">
+            <div>
+                <img src="https://visualizecolor.blob.core.windows.net/ppgpaints/colorspage/browseall.jpg" alt="">
+                <span>Browse All Colors</span>
+            </div>
+            <div>
+                <img src="https://visualizecolor.blob.core.windows.net/ppgpaints/colorspage/collections.jpg" alt="">
+                <span>View Curated Palettes</span>
+            </div>
+        </div>  
+    </div>
+    
+
+</div>
