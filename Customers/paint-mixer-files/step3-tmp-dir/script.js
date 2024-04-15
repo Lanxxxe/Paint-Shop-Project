9721d72a3526caef836befaad1abdbadc555e487
Jay0x13 <dilucrag19@gmail.com>
2024-04-15 23:37:52 +0800

    added step3-tmp-dir folder for step 3 adjustment

---
+++ Customers/paint-mixer-files/step3-tmp-dir/script.js
@@ -0,0 +1,36 @@
+
+function visualizeRoom(imageURL) {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  const img = new Image();
+
+  img.addEventListener('load', () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    const rectData = {
+      x: 0,
+      y: 0,
+      width: img.width,
+      height: img.height
+    };
+
+    drawRect(ctx, rectData);
+    ctx.drawImage(img, 0, 0);
+  }, { once: true });
+
+  img.crossOrigin = "Anonymous";
+  img.src = imageURL;
+}
+
+
+function drawRect(ctx, data) {
+  ctx.beginPath();
+  ctx.rect(data.x, data.y, data.width, data.height);
+  ctx.fillStyle = "rgb(237, 237, 237)";
+  ctx.fill();
+  ctx.closePath();
+}
+
+visualizeRoom("PPG5982V_Thumb.png");
