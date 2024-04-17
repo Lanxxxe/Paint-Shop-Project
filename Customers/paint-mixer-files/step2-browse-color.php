0c945c97a0893f41d40cceddaa3165685a922020
Charles Lance <lancegula05@gmail.com>
2024-04-18 06:23:32 +0800

    Creating Step 2 - Browse Colors

---
+++ Customers/paint-mixer-files/step2-browse-color.php
@@ -0,0 +1,41 @@
+<div class="progress-container">
+    <nav class="progress-bar">
+        <ul class="">
+            <li>
+                <a class="" href="">1</a>
+                <span>Select Photo</span>
+            </li>
+            <li>
+                <a href="">2</a>
+                <span>Select Color</span>
+            </li>
+            <li>
+                <a href="">3</a>
+                <span>Visualize Room</span>
+            </li>
+            <li>
+                <a href="">4</a>
+                <span>Download Paint</span>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="browse-colors-container">
+        <a href="#">
+            < Explore Different Colors</a>
+                <h1>Browse Paint Colors</h1>
+                <span>Color</span>
+
+                <div class="color-container">
+                    <div class="color-list">
+                        <span></span>
+                    </div>
+                    <div>
+                        <img src="https://visualizecolor.blob.core.windows.net/ppgpaints/colorspage/collections.jpg" alt="">
+                        <span>View Curated Palettes</span>
+                    </div>
+                </div>
+    </div>
+
+
+</div>
