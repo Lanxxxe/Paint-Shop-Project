668758c020c3e1d346a3619c9e01582859d77769
Jay0x13 <dilucrag19@gmail.com>
2024-04-20 11:44:52 +0800

    created an objects that make the shapes of objects that color can be changed

--- Customers/paint-mixer-files/step3/script.js
+++ Customers/paint-mixer-files/step3/script.js
@@ -1,4 +1,46 @@
 
+const objects = (function livingRoomObjects() {
+  const obj = {
+    wall: [
+      { x: 181, y: 0 },
+      { x: 181, y: 1.4285707473754883 },
+      { x: 177, y: 3.4285707473754883 },
+      { x: 177, y: 247 },
+      { x: 0, y: 247 },
+      { x: 0, y: 330 },
+      { x: 720.4285888671875, y: 330 },
+      { x: 720.4285888671875, y: 247 },
+      { x: 430, y: 247 },
+      { x: 430, y: 3.4285707473754883 },
+      { x: 428, y: 1.4285707473754883 },
+      { x: 428, y: 0 },
+      { x: 273.14286041259766, y: 0 },
+    ],
+  windows: [
+      [
+        { x: 0, y: 0 },
+        { x: 181, y: 0 },
+        { x: 181, y: 1.2285707473754883 },
+        { x: 177, y: 3.2285707473754883 },
+        { x: 177, y: 247 },
+        { x: 0, y: 247 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { x: 428, y: 0 },
+        { x: 428, y: 1.4285707473754883 },
+        { x: 430, y: 3.4285707473754883 },
+        { x: 430, y: 247 },
+        { x: 720.4285888671875, y: 247 },
+        { x: 720.4285888671875, y: 0 },
+        { x: 428, y: 0 },
+      ],
+    ]
+  };
+
+  return obj;
+})();
+
 function visualizeRoom(imageURL) {
   const canvas = document.getElementById("visual-canvas");
   const ctx = canvas.getContext("2d");
@@ -9,16 +51,11 @@ function visualizeRoom(imageURL) {
     canvas.width = img.width;
     canvas.height = img.height;
 
-    const rectData = {
-      x: 0,
-      y: 0,
-      width: img.width,
-      height: img.height
-    };
-
-    drawRect(ctx, rectData);
+    drawObject(ctx, objects.wall, "lightgreen");
+    drawObject(ctx, objects.windows[0], "lightblue");
+    drawObject(ctx, objects.windows[1], "lightpink");
+  
     ctx.drawImage(img, 0, 0);
-    
   }, { once: true });
 
   img.crossOrigin = "Anonymous";
@@ -26,10 +63,13 @@ function visualizeRoom(imageURL) {
 }
 
 
-function drawRect(ctx, data) {
+function drawObject(ctx, obj, color) {
   ctx.beginPath();
-  ctx.rect(data.x, data.y, data.width, data.height);
-  ctx.fillStyle = "lightseagreen";// "rgb(237, 237, 237)";
+  ctx.moveTo(obj[0].x, obj[0].y);
+  for (let i = 1; i < obj.length; ++i) {
+    ctx.lineTo(obj[i].x, obj[i].y);
+  }
+  ctx.fillStyle = color; 
   ctx.fill();
   ctx.closePath();
 }
