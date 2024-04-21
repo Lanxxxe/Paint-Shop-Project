58c0efd4ba017d79bc2c62645a440a2eb9187baa
Jay0x13 <dilucrag19@gmail.com>
2024-04-21 19:33:31 +0800

    added functionalities in step3

--- Customers/paint-mixer-files/step3/script.js
+++ Customers/paint-mixer-files/step3/script.js
@@ -1,77 +1,184 @@
+class RoomObject {
+  #paths;
+  constructor (paths, fillColor="rgb(0,0,0)", strokeColor="rgb(0,0,0)", strokeWidth=0) {
+    this.#paths = paths;
+    this.fillColor = fillColor ;
+    this.strokeColor = strokeColor;
+    this.strokeWidth = strokeWidth;
+  }
 
-const objects = (function livingRoomObjects() {
-  const obj = {
-    wall: [
-      { x: 181, y: 0 },
-      { x: 181, y: 1.4285707473754883 },
-      { x: 177, y: 3.4285707473754883 },
-      { x: 177, y: 247 },
-      { x: 0, y: 247 },
-      { x: 0, y: 330 },
-      { x: 720.4285888671875, y: 330 },
-      { x: 720.4285888671875, y: 247 },
-      { x: 430, y: 247 },
-      { x: 430, y: 3.4285707473754883 },
-      { x: 428, y: 1.4285707473754883 },
-      { x: 428, y: 0 },
-      { x: 273.14286041259766, y: 0 },
-    ],
-  windows: [
-      [
-        { x: 0, y: 0 },
-        { x: 181, y: 0 },
-        { x: 181, y: 1.2285707473754883 },
-        { x: 177, y: 3.2285707473754883 },
-        { x: 177, y: 247 },
-        { x: 0, y: 247 },
-        { x: 0, y: 0 },
-      ],
-      [
-        { x: 428, y: 0 },
-        { x: 428, y: 1.4285707473754883 },
-        { x: 430, y: 3.4285707473754883 },
-        { x: 430, y: 247 },
-        { x: 720.4285888671875, y: 247 },
-        { x: 720.4285888671875, y: 0 },
-        { x: 428, y: 0 },
-      ],
-    ]
-  };
+  getPaths() {
+    return this.#paths;
+  }
 
-  return obj;
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(this.#paths[0].x, this.#paths[0].y);
+    for (let i = 1; i < this.#paths.length; ++i) {
+      ctx.lineTo(this.#paths[i].x, this.#paths[i].y);
+    }
+    ctx.fillStyle = this.fillColor; 
+    ctx.strokeStyle = this.strokeColor;
+    ctx.lineWidth = this.strokeWidth;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  isPointed(x, y) {
+    let inside = false;
+    for (let i = 0, j = this.#paths.length - 1; i < this.#paths.length; j = i++) {
+      const xi = this.#paths[i].x, yi = this.#paths[i].y;
+      const xj = this.#paths[j].x, yj = this.#paths[j].y;
+
+      const intersect = ((yi > y) !== (yj > y)) &&
+        (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+      if (intersect) inside = !inside;
+    }
+    return inside;
+  }
+
+  static drawRoomObject(ctx, object, fill="rgb(0,0,0)", stroke="rgb(0,0,0)", lineWidth=0) {
+    const paths = object.getPaths();
+    ctx.beginPath();
+    ctx.moveTo(paths[0].x, paths[0].y);
+    for (let i = 1; i < paths.length; ++i) {
+      ctx.lineTo(paths[i].x, paths[i].y);
+    }
+    ctx.fillStyle = fill; 
+    ctx.strokeStyle = stroke;
+    ctx.lineWidth = lineWidth;
+    ctx.fill();
+    ctx.closePath();
+  }
+};
+
+
+const livingRoomObject = (() => {
+  const wall = new RoomObject([
+    { x: 181, y: 0 },
+    { x: 181, y: 1.4285707473754883 },
+    { x: 177, y: 3.4285707473754883 },
+    { x: 177, y: 247 },
+    { x: 0, y: 247 },
+    { x: 0, y: 330 },
+    { x: 720.4285888671875, y: 330 },
+    { x: 720.4285888671875, y: 247 },
+    { x: 430, y: 247 },
+    { x: 430, y: 3.4285707473754883 },
+    { x: 428, y: 1.4285707473754883 },
+    { x: 428, y: 0 },
+    { x: 273.14286041259766, y: 0 },
+  ], "rgb(227, 227, 227)");
+
+  const windowBorder1 = new RoomObject([
+    { x: 0, y: 0 },
+    { x: 181, y: 0 },
+    { x: 181, y: 1.2285707473754883 },
+    { x: 177, y: 3.2285707473754883 },
+    { x: 177, y: 247 },
+    { x: 0, y: 247 },
+    { x: 0, y: 0 },
+  ], "rgb(227, 227, 227)");
+
+  const windowBorder2 = new RoomObject([
+    { x: 428, y: 0 },
+    { x: 428, y: 1.4285707473754883 },
+    { x: 430, y: 3.4285707473754883 },
+    { x: 430, y: 247 },
+    { x: 720.4285888671875, y: 247 },
+    { x: 720.4285888671875, y: 0 },
+    { x: 428, y: 0 },
+  ], "rgb(227, 227, 227)");
+
+  return { wall, windowBorder1, windowBorder2};
 })();
 
-function visualizeRoom(imageURL) {
+
+const bedRoomObject = (() => {
+})();
+
+
+// The current color picked
+let currentColor = "lightgreen";
+
+
+const visualizer = (function() {
   const canvas = document.getElementById("visual-canvas");
   const ctx = canvas.getContext("2d");
-
   const img = new Image();
+  const roomObj = {
+    "../images/living-room.png": livingRoomObject,
+  };
+  let currentRoomObj;
 
-  img.addEventListener('load', () => {
+  const clearCanvas = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  const drawObjects = () => {
+    Object.values(currentRoomObj).forEach((obj) => {
+      obj.draw(ctx);
+    });
+    ctx.drawImage(img, 0, 0);
+  }
+
+  const loadImage = () => {
+    clearCanvas();
     canvas.width = img.width;
     canvas.height = img.height;
+    drawObjects();
+  };
 
-    drawObject(ctx, objects.wall, "lightgreen");
-    drawObject(ctx, objects.windows[0], "lightblue");
-    drawObject(ctx, objects.windows[1], "lightpink");
-  
-    ctx.drawImage(img, 0, 0);
-  }, { once: true });
+  const setImage = (imageURL) => {
+    currentRoomObj = roomObj[imageURL];
+    img.crossOrigin = "Anonymous";
+    img.src = imageURL;
+  };
 
-  img.crossOrigin = "Anonymous";
-  img.src = imageURL;
-}
-
-
-function drawObject(ctx, obj, color) {
-  ctx.beginPath();
-  ctx.moveTo(obj[0].x, obj[0].y);
-  for (let i = 1; i < obj.length; ++i) {
-    ctx.lineTo(obj[i].x, obj[i].y);
+  const hoverOff = () => {
+    clearCanvas();
+    drawObjects();
   }
-  ctx.fillStyle = color; 
-  ctx.fill();
-  ctx.closePath();
-}
 
-visualizeRoom("../images/living-room.png");
+  const hoverEffect = (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    Object.values(currentRoomObj).forEach((obj) => {
+      if (obj.isPointed(x, y)) {
+        clearCanvas();
+        Object.values(currentRoomObj).forEach((obj) => {
+          obj.draw(ctx);
+        })
+        RoomObject.drawRoomObject(ctx, obj, "rgba(70,70,255,0.3)");
+        ctx.drawImage(img, 0, 0);
+      }
+    });
+  };
+
+  const fillObject = (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    Object.values(currentRoomObj).forEach((obj) => {
+      if (obj.isPointed(x, y)) {
+        obj.fillColor = currentColor;
+      }
+    });
+
+    clearCanvas();
+    drawObjects();
+  }
+
+  img.addEventListener('load', loadImage);
+  canvas.addEventListener('mousemove', hoverEffect);
+  canvas.addEventListener('mouseout', hoverOff);
+  canvas.addEventListener('mousedown', fillObject);
+  return { setImage };
+
+})();
+
+
+visualizer.setImage("../images/living-room.png");
